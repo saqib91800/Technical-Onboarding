56d156d795a70b25de30dc88ad4358e01228752a
Muhammad Saqib <tech9180@gmail.com>
2023-11-30 01:49:44 +0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Muhammad Saqib",
+  title: "Website Developer",
+  email: "tech9180@gmail.com",
+  gitHub: "https://github.com/saqib91800",
+  instagram: "https://www.instagram.com/they_call_me_rajput_/",
+  linkedIn: "https://www.linkedin.com/in/hsaqib-nisar-81b422171/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/earth.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Earth background image!";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Personal website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is my website, which I created. ",
+    url: "https://techtonixinternational.agency",
   },
   {
-    title: "Web Development for Beginners",
+    title: "My LinkedIn Profile",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Here I added my linkedin profile.",
+    url: "https://www.linkedin.com/in/hsaqib-nisar-81b422171/",
   },
   {
-    title: "My Resume Site",
+    title: "My Fiverr profile",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "I'm a level 2 seller on Fiverr",
+    url: "https://www.fiverr.com/dany_daneal",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "GitHub Account",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is my Github account, I didnt work on it more but I will make it good soon.",
+    url: "https://github.com/saqib91800",
   },
 ];
 
